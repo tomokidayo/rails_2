8561d1ebfd7c69ce3bfd5b0cd3cdc3bad5a09c59
tomokidayo <toblue0905@gmail.com>
2026-04-07 14:27:56 +0900

    UI整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  # app/models/user.rb
   has_many :rooms
   has_many :reservations
   has_one_attached :icon
 
   # バリデーションの追加
-
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
